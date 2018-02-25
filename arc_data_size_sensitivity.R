61469272a511dead2ad0b7a609e4ed9c158a7bf0
kliegr <kliegr@gmail.com>
2018-02-25 14:16:21 +0100

    added arulesCBA

--- arc_data_size_sensitivity.R
+++ arc_data_size_sensitivity.R
@@ -1,21 +1,28 @@
+#library(devtools)
+#install_github('ianjjohnson/arulesCBA', ref="time-trials", force=TRUE)
+#install_github('jaroslav-kuchar/rCBA', ref="develop")
+
 library(arc)
 library(rCBA)
+library(arulesCBA)
 dataset_path <- "data/folds/train/lymph0.csv"
 #data with discretization applied by external preprocessing
 trainFold <- utils::read.csv(dataset_path, header = TRUE, check.names = FALSE)
 trainFold[,9] <- discretize(trainFold[,9], "frequency", categories=3)
 trainFold[,10] <- discretize(trainFold[,10], "frequency", categories=3)
 trainFold[,18] <- discretize(trainFold[,18], "frequency", categories=3)
+disc.data <- lapply(trainFold, levels)
 classAtt <- "class"
 outputFileName <- "result/arc-data-size.csv"
 appearance <- arc::getAppearance(trainFold, classAtt)
-write(paste("dataset,input rows,input rules,output_rules_arc,output_rules_rcba,time_arc,time_rcba"), file = outputFileName,
+write(paste("dataset,input rows,input rules,output_rules_arc,output_rules_acba,output_rules_rcba,time_arc,time_acba,time_rcba"), file = outputFileName,
       ncolumns = 1,
       append = FALSE, sep = ",")
 
 rule_count=100
 #we gradually increase the number of input rows and observe how run time changes
 trainFold_oversampled <- trainFold
+number_of_iterations<=1
 for (i in seq(1,1000))
 {
   # double the dataset on each iteration
@@ -29,25 +36,38 @@ for (i in seq(1,1000))
 #arc start
     # we do ten iterations to have a more robust estimate
     ptm <- proc.time()
-    for (j in 1:10)
+    for (j in 1:number_of_iterations)
     {
         rmCBA <- cba_manual(trainFold_oversampled,subs_rules, txns_discr, appearance$rhs,
                     classAtt, cutp= list(), pruning_options=NULL)
     }
     proctime<- proc.time() - ptm
-    dur_arc<-proctime[3]/10
+    dur_arc<-proctime[3]/number_of_iterations
 #arc end
+    
 #rCBA start
     ptm <- proc.time()
-    for (j in 1:10)
+    for (j in 1:number_of_iterations)
     {
       rmRCBA <- rCBA::pruning(trainFold_oversampled, subs_rules, method="m2cba")
     }
     proctime<- proc.time() - ptm
-    dur_rcba<-proctime[3]/10
-
+    dur_rcba<-proctime[3]/number_of_iterations
 #rCBA end
-    write(paste(dataset_path, nrow(trainFold_oversampled), length(subs_rules), length(rmCBA@rules), nrow(rmRCBA), dur_arc, dur_rcba, sep = ","), file = outputFileName,
+#arulesCBA start      
+    ptm <- proc.time()
+    for (j in 1:number_of_iterations)
+    {
+      message("starting arulesCBA")
+      rmArulesCBA <- arulesCBA::CBA.internal(subs_rules, txns_discr, classAtt, disc.data, method="CBA")
+      message(paste("number of input rules:",rule_count,", pruned rules in arulesCBA model:", length(rmArulesCBA$rules)))
+      message("arulesCBA finished")
+    }
+    proctime<- proc.time() - ptm
+    dur_acba<-proctime[3]/number_of_iterations
+    message(paste("acba finished"))
+#arulesCBA end              
+        write(paste(dataset_path, nrow(trainFold_oversampled), length(subs_rules), length(rmCBA@rules), nrow(rmRCBA), length(rmArulesCBA$rules), dur_arc, dur_rcba, dur_acba, sep = ","), file = outputFileName,
           ncolumns = 1,
           append = TRUE, sep = ",")
 }
