0e9ffebaafcf68f8ebdb005dd650eea517221353
kliegr <kliegr@gmail.com>
2018-02-26 11:35:09 +0100

    arulesCBA included

--- arc_data_size_sensitivity.R
+++ arc_data_size_sensitivity.R
@@ -44,17 +44,7 @@ for (i in seq(1,1000))
     proctime<- proc.time() - ptm
     dur_arc<-proctime[3]/number_of_iterations
 #arc end
-#qcba start
-# we do ten iterations to have a more robust estimate
-ptm <- proc.time()
-for (j in 1:number_of_iterations)
-{
-  rmCBA <- cba_manual(trainFold_oversampled,subs_rules, txns_discr, appearance$rhs,
-                      classAtt, cutp= list(), pruning_options=NULL)
-}
-proctime<- proc.time() - ptm
-dur_arc<-proctime[3]/number_of_iterations
-#qcba end    
+ 
 #rCBA start
     ptm <- proc.time()
     for (j in 1:number_of_iterations)
