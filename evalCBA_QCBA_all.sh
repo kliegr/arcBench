e005fbe41856d08b1d80d7ce2d7bd608b4ce30a7
kliegr <6094393+kliegr@users.noreply.github.com>
2020-11-22 14:58:02 +0100

    Evaluation is no longer run in parallel
    This can improve time estimates.

--- evalCBA_QCBA_all.sh
+++ evalCBA_QCBA_all.sh
@@ -1,8 +1,8 @@
-nohup Rscript evalCBA_QCBA_all.R 117 & # baseline
-nohup Rscript evalCBA_QCBA_all.R 120 & # only refit
-nohup Rscript evalCBA_QCBA_all.R 114 & # + attribute removal
-nohup Rscript evalCBA_QCBA_all.R 42 &  # + trimming
-nohup Rscript evalCBA_QCBA_all.R 186 & # + extend
-nohup Rscript evalCBA_QCBA_all.R 198 & # + postpruning
-nohup Rscript evalCBA_QCBA_all.R 196 & # + default rule - transactio
-nohup Rscript evalCBA_QCBA_all.R 197 & # + default rule - range
+Rscript evalCBA_QCBA_all.R 117  # baseline
+Rscript evalCBA_QCBA_all.R 120  # only refit
+Rscript evalCBA_QCBA_all.R 114  # + attribute removal
+Rscript evalCBA_QCBA_all.R 42   # + trimming
+Rscript evalCBA_QCBA_all.R 186  # + extend
+Rscript evalCBA_QCBA_all.R 198  # + postpruning
+Rscript evalCBA_QCBA_all.R 196  # + default rule - transactio
+Rscript evalCBA_QCBA_all.R 197  # + default rule - range
